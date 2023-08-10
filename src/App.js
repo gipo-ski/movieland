06fe8ed01924405012aa7320e8d93c1f9f5c6967
gipo-ski <koladiji@gmail.com>
2023-08-10 23:21:52 +0100

    added MovieCard.jsx and updated App

--- src/App.js
+++ src/App.js
@@ -1,21 +1,64 @@
-// import logo from './logo.svg';
-import {useState} from 'react';
-import Person from "./Person";
+import React from "react";
+import { useEffect } from "react";
 import "./App.css";
+import "./search.svg";
+
+const API_URL = "https://www.omdbapi.com?apikey=22de76cb";
+
+const movie1 = {
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BOGE4NzU1YTAtNzA3Mi00ZTA2LTg2YmYtMDJmMThiMjlkYjg2XkEyXkFqcGdeQXVyNTgzMDMzMTg@._V1_SX300.jpg",
+  Title: "Thor",
+  Type: "movie",
+  Year: "2011",
+  imdbID: "tt0800369",
+};
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
-  
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Thor");
+  }, []);
+
   return (
-    <>
-      <Person name={"John"} surname={"Doe"} age={30} />
-      <div className="App">
-        <button onClick={() => { setCounter((prevCount) => prevCount - 1); }}>-</button>
-        <h1>{counter}</h1>
-        <button onClick= {() => {setCounter((prevCount) => prevCount + 1);}} >+</button>
+    <div className="app">
+      <h1>MovieLand</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value="Spiderman"
+          onChange={() => {}}
+        />
+        <img src="" alt="" onClick={() => {}} />
       </div>
-    </>
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img
+              src={ movie1.Poster !== 'N/A' ? movie1.Poster : 'http://via.placeholder.com/400'}
+              alt={movie1.Title}
+            />
+          </div>
+
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default App;
+
+//  <>
+//     <h1>App</h1>
+//     <MovieCard />
+
+//   </>
---
+++ src/MovieCard.jsx
@@ -0,0 +1,18 @@
+// import React from 'react';
+// import App from './App';
+
+// const MovieCard = (props) => {
+//     return (
+//       <div className="container">
+//         <div>
+//           <h2>{props.Title}</h2>
+//             </div>
+//             <div>
+//                 <img src={props.Poster} alt={404} />
+//             </div>
+//       </div>
+//     );
+
+// };
+
+// export default MovieCard;
